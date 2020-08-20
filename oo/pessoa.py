384da3f4476901450743dafae775546fbdd16be9
tarciso <tarciso@nte.ap.gov.br>
2020-08-19 23:31:29 -0300

    Criado atributos de instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,8 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=None):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 if __name__ == '__main__':
@@ -6,6 +10,16 @@ if __name__ == '__main__':
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Paulo Tarciso'
+    p.idade = 56
+    print(p.nome)
+    print(p.idade)
+
+
+
+
+
 
 
 

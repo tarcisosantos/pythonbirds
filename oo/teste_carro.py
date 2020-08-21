e1ab92de610a1e0b13d88c535f6edcc53dda099c
tarciso <tarciso@nte.ap.gov.br>
2020-08-20 22:50:20 -0300

    Implementando os Teste e Renomenado arquivo teste do birds

--- oo/teste_carro.py
+++ oo/teste_carro.py
@@ -1,9 +1,11 @@
 from unittest import TestCase
+from oo.carro import Motor
 
 class CarroTestCase(TestCase):
     def teste_velocidade_inicial(self):
         motor = Motor()
         self.assertEqual(0, motor.velocidade)
+
     def teste_acelerar(self):
         motor = Motor()
         motor.acelerar()
